2f7eb78e1c5efb784e2f6d9840fe6179a8b51d20
Alfonsina <alfonsina.l@gmail.com>
2023-02-02 17:52:26 -0300

    feat(seed-data): add seed data product information

--- seed-data/index.ts
+++ seed-data/index.ts
@@ -1,6 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { products } from './data';
 
-export async function insertSeedData(ks: any) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+export async function insertSeedData(ks: any): Promise<void> {
   // Keystone API changed, so we need to check for both versions to get keystone
   const keystone = ks.keystone || ks;
   const adapter = keystone.adapters?.MongooseAdapter || keystone.adapter;
@@ -9,13 +12,14 @@ export async function insertSeedData(ks: any) {
   const { mongoose } = adapter;
   for (const product of products) {
     console.log(`  🛍️ Adding Product: ${product.name}`);
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     const { _id } = await mongoose
-    .model('ProductImage')
-    .create({ image: product.photo, altText: product.description });
+      .model('ProductImage')
+      .create({ image: product.photo, altText: product.description });
     product.photo = _id;
     await mongoose.model('Product').create(product);
   }
   console.log(`✅ Seed Data Inserted: ${products.length} Products`);
-  console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+  console.log('👋 Please start the process with `yarn dev` or `npm run dev`');
   process.exit();
 }
